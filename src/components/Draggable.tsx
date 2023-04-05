f9154f2e8ac878d366d942a37c923238a9976c12
angus <angussbjohnson@gmail.com>
2023-04-05 22:49:43 +1000

    Avoid jumps when starting dragging

--- src/components/Draggable.tsx
+++ src/components/Draggable.tsx
@@ -1,4 +1,10 @@
-import React, { ReactNode, useEffect, useMemo, useRef } from "react";
+import React, {
+  ReactNode,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+} from "react";
 import { useThree } from "@react-three/fiber";
 import { useDrag } from "react-use-gesture";
 import { Group, Plane, Raycaster, Vector2, Vector3 } from "three";
@@ -20,6 +26,7 @@ export const Draggable: React.FC<DraggableProps> = ({
   // Raycaster and plane used to project the mouse movements from the camera onto the plane
   const raycaster = useMemo(() => new Raycaster(), []);
   const planeRef = useRef(new Plane(new Vector3(0, 0, 1)));
+  const mouseObjectOffset = useRef(new Vector3(0, 0, 0));
 
   useEffect(() => {
     if (objectRef.current) {
@@ -27,29 +34,40 @@ export const Draggable: React.FC<DraggableProps> = ({
     }
   }, []);
 
+  const screenToSpace = useCallback((x: number, y: number): Vector3 => {
+    const screenCoords = new Vector2(
+      (x / size.width) * 2 - 1,
+      -(y / size.height) * 2 + 1
+    );
+
+    const intersection = new Vector3();
+    raycaster.setFromCamera(screenCoords, camera);
+    raycaster.ray.intersectPlane(planeRef.current, intersection);
+
+    return intersection;
+  }, []);
+
   const bind = useDrag(({ xy: [x, y], first, last }) => {
     if (first) {
       setCurrentlyDragging(false);
+
       const cameraDirection = new Vector3();
       camera.getWorldDirection(cameraDirection);
       planeRef.current.setFromNormalAndCoplanarPoint(
         cameraDirection,
         objectRef.current?.position || new Vector3(0, 0, 0)
       );
+
+      mouseObjectOffset.current =
+        objectRef.current?.position.clone().sub(screenToSpace(x, y)) ||
+        new Vector3(0, 0, 0);
     }
     if (last) setCurrentlyDragging(true);
 
     if (objectRef.current) {
-      const screenCoords = new Vector2(
-        (x / size.width) * 2 - 1,
-        -(y / size.height) * 2 + 1
+      objectRef.current.position.copy(
+        screenToSpace(x, y).add(mouseObjectOffset.current)
       );
-
-      raycaster.setFromCamera(screenCoords, camera);
-      const intersection = new Vector3();
-      raycaster.ray.intersectPlane(planeRef.current, intersection);
-
-      objectRef.current.position.copy(intersection);
     }
   }) as () => {};
 
